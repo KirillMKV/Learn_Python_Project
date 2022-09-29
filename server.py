cec573090dae5dc648917fa7d19a6e8da609377f
Daria Kovregina <Dkovregina0898@gmail.com>
2022-09-29 23:17:09 +0300

    Log in

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+from login_form import LoginForm
+from flask import render_template
+
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+@app.route("/")
+def login_form():
+    title = "Log in"
+    login_form = LoginForm()
+    return render_template('login.html', page_title=title, form=login_form)
+
+
+if __name__ == '__main__':
+    app.run()
